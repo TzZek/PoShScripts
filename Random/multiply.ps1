c53042e364c4cb6f57ea5d30bf49d44a05af7b10
TzZek <tzzek@pm.me>
2021-11-03 05:03:59 -0700

    last one for now - added multiply ps1

---
+++ Random/multiply.ps1
@@ -0,0 +1,7 @@
+[int]$num1 = Read-Host -Prompt "Enter the first number."
+[int]$num2 = Read-Host -Prompt "Enter the second number."
+
+clear
+
+Write-Host("Num1: $num1 and Num2: $num1")
+$num1 * $num2

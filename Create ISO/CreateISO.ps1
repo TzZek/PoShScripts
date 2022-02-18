3adbc50e5fadf8bcbbbcf1885f314341037ece23
TzZek <tzzek@pm.me>
2022-02-17 23:31:58 -0800

    project start sorta

--- Create ISO/CreateISO.ps1
+++ Create ISO/CreateISO.ps1
@@ -1,3 +1,8 @@
+# Credits:
+
+#New-IsoFile AUTHOR: Chris Wu LASTEDIT: 03/23/2016 14:46:50
+# GUI Designed by Derek Kirby - 2/17/2022 
+
 # Init PowerShell Gui
 Add-Type -AssemblyName System.Windows.Forms
 
@@ -36,15 +41,98 @@ $CreateIsoBtn.Visible           = $true
 $ISOConverter.controls.AddRange(@($FolderSelectBtn, $CreateIsoBtn))
 
 
-$FolderSelectBtn.Add_Click({ SelectFolder })
-
+$FolderSelectBtn.Add_Click({ SelectFolder }) 
 # required functions
-function SelectFolder { 
-Add-Type -AssemblyName System.Windows.Forms
-$ofd=New-Object System.Windows.Forms.OpenFileDialog
-$ofd.InitialDirectory = "\\$env:Computername\c$\Windows"
-$ofd.ShowDialog()
+Function SelectFolder($initialDirectory) {
+    Add-Type -AssemblyName System.Windows.Forms
+    $browser = New-Object System.Windows.Forms.FolderBrowserDialog
+    $null = $browser.ShowDialog()
+    $UserSelectedPath = $browser.SelectedPath
+    write-host("You selected the Path: "+ $UserSelectedPath)
 }
+
+$CreateIsoBtn.Add_Click({ New-IsoFile })
+
+
+function New-IsoFile 
+{  
+
+  [CmdletBinding(DefaultParameterSetName='Source')]Param( 
+    [parameter(Position=1,Mandatory=$true,ValueFromPipeline=$true, ParameterSetName='Source')]$Source,  
+    [parameter(Position=2)][string]$Path = "$env:C:\Users\dk\Desktop\ISOs\$((Get-Date).ToString('yyyyMMdd-HHmmss.ffff')).iso",  
+    [ValidateScript({Test-Path -LiteralPath $_ -PathType Leaf})][string]$BootFile = $null, 
+    [ValidateSet('CDR','CDRW','DVDRAM','DVDPLUSR','DVDPLUSRW','DVDPLUSR_DUALLAYER','DVDDASHR','DVDDASHRW','DVDDASHR_DUALLAYER','DISK','DVDPLUSRW_DUALLAYER','BDR','BDRE')][string] $Media = 'DVDPLUSRW_DUALLAYER', 
+    [string]$Title = (Get-Date).ToString("yyyyMMdd-HHmmss.ffff"),  
+    [switch]$Force, 
+    [parameter(ParameterSetName='Clipboard')][switch]$FromClipboard 
+  ) 
+  
+  Begin {  
+    ($cp = new-object System.CodeDom.Compiler.CompilerParameters).CompilerOptions = '/unsafe' 
+    if (!('ISOFile' -as [type])) {  
+      Add-Type -CompilerParameters $cp -TypeDefinition @'
+public class ISOFile  
+{ 
+  public unsafe static void Create(string Path, object Stream, int BlockSize, int TotalBlocks)  
+  {  
+    int bytes = 0;  
+    byte[] buf = new byte[BlockSize];  
+    var ptr = (System.IntPtr)(&bytes);  
+    var o = System.IO.File.OpenWrite(Path);  
+    var i = Stream as System.Runtime.InteropServices.ComTypes.IStream;  
+   
+    if (o != null) { 
+      while (TotalBlocks-- > 0) {  
+        i.Read(buf, BlockSize, ptr); o.Write(buf, 0, bytes);  
+      }  
+      o.Flush(); o.Close();  
+    } 
+  } 
+}  
+'@  
+    } 
+   
+    if ($BootFile) { 
+      if('BDR','BDRE' -contains $Media) { Write-Warning "Bootable image doesn't seem to work with media type $Media" } 
+      ($Stream = New-Object -ComObject ADODB.Stream -Property @{Type=1}).Open()  # adFileTypeBinary 
+      $Stream.LoadFromFile((Get-Item -LiteralPath $BootFile).Fullname) 
+      ($Boot = New-Object -ComObject IMAPI2FS.BootOptions).AssignBootImage($Stream) 
+    } 
+  
+    $MediaType = @('UNKNOWN','CDROM','CDR','CDRW','DVDROM','DVDRAM','DVDPLUSR','DVDPLUSRW','DVDPLUSR_DUALLAYER','DVDDASHR','DVDDASHRW','DVDDASHR_DUALLAYER','DISK','DVDPLUSRW_DUALLAYER','HDDVDROM','HDDVDR','HDDVDRAM','BDROM','BDR','BDRE') 
+  
+    Write-Verbose -Message "Selected media type is $Media with value $($MediaType.IndexOf($Media))"
+    ($Image = New-Object -com IMAPI2FS.MsftFileSystemImage -Property @{VolumeName=$Title}).ChooseImageDefaultsForMediaType($MediaType.IndexOf($Media)) 
+   
+    if (!($Target = New-Item -Path $Path -ItemType File -Force:$Force -ErrorAction SilentlyContinue)) { Write-Error -Message "Cannot create file $Path. Use -Force parameter to overwrite if the target file already exists."; break } 
+  }  
+  
+  Process { 
+    if($FromClipboard) { 
+      if($PSVersionTable.PSVersion.Major -lt 5) { Write-Error -Message 'The -FromClipboard parameter is only supported on PowerShell v5 or higher'; break } 
+      $Source = Get-Clipboard -Format FileDropList 
+    } 
+  
+    foreach($item in $Source) { 
+      if($item -isnot [System.IO.FileInfo] -and $item -isnot [System.IO.DirectoryInfo]) { 
+        $item = Get-Item -LiteralPath $item
+      } 
+  
+      if($item) { 
+        Write-Verbose -Message "Adding item to the target image: $($item.FullName)"
+        try { $Image.Root.AddTree($item.FullName, $true) } catch { Write-Error -Message ($_.Exception.Message.Trim() + ' Try a different media type.') } 
+      } 
+    } 
+  } 
+  
+  End {  
+    if ($Boot) { $Image.BootImageOptions=$Boot }  
+    $Result = $Image.CreateResultImage()  
+    [ISOFile]::Create($Target.FullName,$Result.ImageStream,$Result.BlockSize,$Result.TotalBlocks) 
+    Write-Verbose -Message "Target image ($($Target.FullName)) has been created"
+    $Target
+  } 
+}  
 
 # Display the form
 [void]$ISOConverter.ShowDialog()
--- psexec_gui/psexec_gui.ps1
+++ psexec_gui/psexec_gui.ps1
@@ -6,3 +6,42 @@
 # checkboxes (add switches like -force)
 # log all commandsthat go across the network
 
+# Init PowerShell Gui
+Add-Type -AssemblyName System.Windows.Forms
+
+# Create new form
+$PSExec_Commander                    = New-Object system.Windows.Forms.Form
+# Define the size, title and background color
+$PSExec_Commander.ClientSize         = '400,400'
+$PSExec_Commander.text               = "PSExec Commander"
+$PSExec_Commander.BackColor          = "#ededed"
+$PSExec_Commander.FormBorderStyle    = 'FixedDialog'
+$PSExec_Commander.MaximizeBox        = $false
+$PSExec_Commander.startposition    = "centerscreen"
+
+# Tab Stuff TODO
+$TabControl = New-Object System.Windows.Forms.TabControl
+
+$tabPage1 = New-Object System.Windows.Forms.TabPage
+$tabPage1.Name = "Tab1"
+$tabPage1.Text = "Tab1"
+
+$tabPage2 = New-Object System.Windows.Forms.TabPage
+$tabPage2.Name = "Tab2"
+$tabPage2.Text = "Tab2"
+
+$TabControl.TabPages.Add($tabPage1)
+$TabControl.TabPages.Add($tabPage2) 
+
+$TabControl.SelectedIndexChanged($TabControl_SelectedIndexChanged)
+
+$TabControl_SelectedIndexChanged
+{
+    ({$selectedTab = $TabControl.SelectedTab;
+
+    [System.Windows.Forms.MessageBox]::Show($selectedTab)})
+}
+$PSExec_Commander.Controls.Add($TabControl)
+
+# Display the form
+[void]$PSExec_Commander.ShowDialog()

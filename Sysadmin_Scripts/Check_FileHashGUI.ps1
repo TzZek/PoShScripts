741392c28ace3a0f127f0e80ff793d9b72fa16ad
Derek B Kirby <tzzek@pm.me>
2024-04-15 12:41:58 -0500

    test dark theme

--- Sysadmin_Scripts/Check_FileHashGUI.ps1
+++ Sysadmin_Scripts/Check_FileHashGUI.ps1
@@ -12,7 +12,7 @@ function Get-FileHashCustom {
     return $hash
 }
 
-# Ensure $script:path is declared at the top to have script-wide scope
+# Variable to hold the selected file path
 $script:path = $null
 
 # Create the form
@@ -20,12 +20,14 @@ $form = New-Object System.Windows.Forms.Form
 $form.Text = 'Drag and Drop File with Hash Check'
 $form.Size = New-Object System.Drawing.Size(500,200)
 $form.StartPosition = 'CenterScreen'
+$form.BackColor = [System.Drawing.Color]::FromArgb(45, 45, 48) # Dark Gray Background
 
 # Label for instructions
 $label = New-Object System.Windows.Forms.Label
 $label.Location = New-Object System.Drawing.Point(10,10)
 $label.Size = New-Object System.Drawing.Size(480,20)
 $label.Text = 'Drag and drop a file onto this window, then enter the hash you want to check.'
+$label.ForeColor = [System.Drawing.Color]::WhiteSmoke # Light text color
 $form.Controls.Add($label)
 
 # ComboBox for Hash Algorithm Selection
@@ -35,6 +37,8 @@ $comboBox.Size = New-Object System.Drawing.Size(200,20)
 $comboBox.DropDownStyle = [System.Windows.Forms.ComboBoxStyle]::DropDownList
 $comboBox.Items.AddRange(@("MD5", "SHA1", "SHA256", "SHA512"))
 $comboBox.SelectedIndex = 0
+$comboBox.BackColor = [System.Drawing.Color]::FromArgb(30, 30, 30) # Darker Gray for ComboBox
+$comboBox.ForeColor = [System.Drawing.Color]::WhiteSmoke # Light text color
 $form.Controls.Add($comboBox)
 
 # Label for File Name
@@ -42,12 +46,15 @@ $fileNameLabel = New-Object System.Windows.Forms.Label
 $fileNameLabel.Location = New-Object System.Drawing.Point(10,70)
 $fileNameLabel.Size = New-Object System.Drawing.Size(480,20)
 $fileNameLabel.Text = 'No file selected'
+$fileNameLabel.ForeColor = [System.Drawing.Color]::WhiteSmoke # Light text color
 $form.Controls.Add($fileNameLabel)
 
 # TextBox for Hash Input
 $textBox = New-Object System.Windows.Forms.TextBox
 $textBox.Location = New-Object System.Drawing.Point(10,100)
 $textBox.Size = New-Object System.Drawing.Size(350,20)
+$textBox.BackColor = [System.Drawing.Color]::FromArgb(30, 30, 30) # Darker Gray for TextBox
+$textBox.ForeColor = [System.Drawing.Color]::WhiteSmoke # Light text color
 $form.Controls.Add($textBox)
 
 # Button to Check Hash
@@ -55,12 +62,15 @@ $button = New-Object System.Windows.Forms.Button
 $button.Location = New-Object System.Drawing.Point(370,100)
 $button.Size = New-Object System.Drawing.Size(100,20)
 $button.Text = 'Check Hash'
+$button.BackColor = [System.Drawing.Color]::FromArgb(104, 33, 122) # A darker shade for Button
+$button.ForeColor = [System.Drawing.Color]::WhiteSmoke # Light text color
 $form.Controls.Add($button)
 
 # Label for Result
 $resultLabel = New-Object System.Windows.Forms.Label
 $resultLabel.Location = New-Object System.Drawing.Point(10,130)
 $resultLabel.Size = New-Object System.Drawing.Size(480,20)
+$resultLabel.ForeColor = [System.Drawing.Color]::WhiteSmoke # Light text color
 $form.Controls.Add($resultLabel)
 
 # Function to handle file drop
